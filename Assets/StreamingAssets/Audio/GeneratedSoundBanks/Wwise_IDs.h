8453615b19dc1cc666938b5383fda85612574087
h_Zben <bensonzhao2002@gmail.com>
2026-03-28 19:09:44 +0800

    Wwise Streaming

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,182 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BAT_FLYAGGRO = 648572810U;
+        static const AkUniqueID BATTERY_PERISH = 3430220816U;
+        static const AkUniqueID CHECKPOINT_HEAL = 3839657096U;
+        static const AkUniqueID CHECKPOINT_HEAL_LOOP = 3325201609U;
+        static const AkUniqueID CHECKPOINT_HEAL_STOP = 1311577255U;
+        static const AkUniqueID EMPHIDIAN_SHOOT = 1829428236U;
+        static const AkUniqueID ENEMY_PERISH = 776453037U;
+        static const AkUniqueID ENEMY_SWEEP_ATTACK = 1479078661U;
+        static const AkUniqueID FURNACE_LAUNCH = 283323471U;
+        static const AkUniqueID FURNACE_LOOP = 3076264760U;
+        static const AkUniqueID GATLING_BIGFIRE = 248788380U;
+        static const AkUniqueID GATLING_SMALLFIRE = 3729333003U;
+        static const AkUniqueID LASER_BEAM = 733612088U;
+        static const AkUniqueID LASER_BEAMALT = 1474345713U;
+        static const AkUniqueID LIGHTNINGCHAIN_CONNECTOR = 1493575816U;
+        static const AkUniqueID LIGHTNINGCHAIN_LAUNCH = 2488834650U;
+        static const AkUniqueID MANA_CELL_FILL = 4113127017U;
+        static const AkUniqueID MANA_SUFFICIENCY_PLAY = 1503682374U;
+        static const AkUniqueID MANA_SUFFICIENCY_STOP = 3313107000U;
+        static const AkUniqueID MUSIC_PLAY = 202194903U;
+        static const AkUniqueID MUSIC_STOP = 3227181061U;
+        static const AkUniqueID PILLAR_RISE = 4067478631U;
+        static const AkUniqueID PLAGUE_BUBBLE = 2035004424U;
+        static const AkUniqueID PLAGUE_SHOT = 2226036498U;
+        static const AkUniqueID SAVAGE_CUTSCENE_DUST = 66490368U;
+        static const AkUniqueID SAVAGE_CUTSCENE_ROAR = 1628602608U;
+        static const AkUniqueID SAVAGE_CUTSCENE_RUMBLE = 2661203955U;
+        static const AkUniqueID SAVAGE_METEOR_FALL = 2390516159U;
+        static const AkUniqueID SAVAGE_METEOR_RISE = 627988779U;
+        static const AkUniqueID SAVAGE_METEOR_SLAM = 2477439771U;
+        static const AkUniqueID SAVAGE_PERISH = 2927270538U;
+        static const AkUniqueID SAVAGE_ROAM = 1203288774U;
+        static const AkUniqueID SAVAGE_SLAM = 4208607350U;
+        static const AkUniqueID SAVAGE_SPIN = 4210578753U;
+        static const AkUniqueID SAVAGE_SPIN_EARTH_PLAY = 2012794953U;
+        static const AkUniqueID SAVAGE_SPIN_EARTH_STOP = 3535315055U;
+        static const AkUniqueID SAVAGE_SPIN_END = 62144163U;
+        static const AkUniqueID SAVAGE_SPIN_START = 1697413752U;
+        static const AkUniqueID SCARAMITE_AGGRO = 3158569827U;
+        static const AkUniqueID SENTINEL_CHARGE_ATTACK = 3654774999U;
+        static const AkUniqueID SENTINEL_CHARGE_WARN = 3598361267U;
+        static const AkUniqueID SENTINEL_SWEEP_WARN = 215180023U;
+        static const AkUniqueID SIFTLING_TORNADO = 3724824085U;
+        static const AkUniqueID SLITHER_SWEEP_WARN = 2724729948U;
+        static const AkUniqueID SLITHER_ZIG_ATTACK = 2231631038U;
+        static const AkUniqueID SLITHER_ZIG_WARN = 1911758058U;
+        static const AkUniqueID SNIPER_FIRE = 2657820951U;
+        static const AkUniqueID SOVEREIGN_ACTIVATE = 3855990057U;
+        static const AkUniqueID SOVEREIGN_COLLAPSE_VOICE = 2396538904U;
+        static const AkUniqueID SOVEREIGN_LASER_LOOP_PLAY = 2398591641U;
+        static const AkUniqueID SOVEREIGN_LASER_LOOP_STOP = 694689247U;
+        static const AkUniqueID SOVEREIGN_LASER_TRIGGER_PLAY = 1342240583U;
+        static const AkUniqueID SOVEREIGN_PAW = 1473386410U;
+        static const AkUniqueID SOVEREIGN_ROAR = 4068999422U;
+        static const AkUniqueID SOVEREIGN_STATIC_VOICE = 1686102659U;
+        static const AkUniqueID SOVEREIGN_SWIPE_VOICE = 3134934187U;
+        static const AkUniqueID STAIR_RISE = 656549100U;
+        static const AkUniqueID STOPALL = 3086540886U;
+        static const AkUniqueID SUMMON_COLLAPSE = 4063454882U;
+        static const AkUniqueID SUMMON_INVALID = 535167848U;
+        static const AkUniqueID SUMMON_PLACE = 4109110236U;
+        static const AkUniqueID SUMMON_SELECT = 3328317015U;
+        static const AkUniqueID SUMMON_SWITCH = 3350963763U;
+        static const AkUniqueID TEMPLE_QUAKE = 79843292U;
+        static const AkUniqueID TEMPORAL_REVERSE = 4267114648U;
+        static const AkUniqueID TOWER_DAMAGE = 4040281772U;
+        static const AkUniqueID WATERFALL_LOOP = 1873129830U;
+        static const AkUniqueID WIND_BURST = 118255496U;
+        static const AkUniqueID YAN_DAMAGE = 1037416985U;
+        static const AkUniqueID YAN_DODGE = 2534221411U;
+        static const AkUniqueID YAN_HEAL = 3381015472U;
+        static const AkUniqueID YAN_MELEE = 1527085362U;
+        static const AkUniqueID YAN_TELEPORT = 1881281259U;
+        static const AkUniqueID YAN_TORNADO = 1315475245U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CHECKPOINT = 612075679U;
+                static const AkUniqueID COMBAT_BRIMSTONE = 3328100817U;
+                static const AkUniqueID COMBAT_CATACOMB = 2449594894U;
+                static const AkUniqueID COMBAT_SOVEREIGN = 1968481852U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID STORY = 1686739444U;
+            } // namespace STATE
+        } // namespace MUSIC
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace MANA_SUFFICIENCY
+        {
+            static const AkUniqueID GROUP = 2948931413U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID INSUFFICIENT = 2362687100U;
+                static const AkUniqueID SUFFICIENT = 1041257717U;
+            } // namespace SWITCH
+        } // namespace MANA_SUFFICIENCY
+
+        namespace SIFTLING_TORNADO
+        {
+            static const AkUniqueID GROUP = 3724824085U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FIRE = 2678880713U;
+                static const AkUniqueID NORMAL = 1160234136U;
+                static const AkUniqueID WATER = 2654748154U;
+                static const AkUniqueID WIND = 1537061107U;
+            } // namespace SWITCH
+        } // namespace SIFTLING_TORNADO
+
+        namespace TOWER_DAMAGE
+        {
+            static const AkUniqueID GROUP = 4040281772U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FIRE = 2678880713U;
+                static const AkUniqueID NULL = 784127654U;
+                static const AkUniqueID PHYSICAL = 3599694366U;
+                static const AkUniqueID POISON = 1413156513U;
+                static const AkUniqueID SHOCK = 2836875581U;
+            } // namespace SWITCH
+        } // namespace TOWER_DAMAGE
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK = 1661994096U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID BOSS = 1560169506U;
+        static const AkUniqueID CUTSCENE = 1182958561U;
+        static const AkUniqueID ENEMY = 2299321487U;
+        static const AkUniqueID HEALTH = 3677180323U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MOB = 1082004793U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID TOWER = 1737926756U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID YAN = 912360423U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
